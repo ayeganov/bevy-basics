57785ff7700c089b73a353495f54af0556828b79
Aleksandr Yeganov <ayeganov@cisco.com>
2024-02-14 14:07:59 -0500

    add spaceship vision

--- episode-5/src/spaceship.rs
+++ episode-5/src/spaceship.rs
@@ -1,13 +1,25 @@
-use bevy::prelude::*;
+use bevy::core_pipeline::clear_color::ClearColorConfig;
+use bevy::{
+    prelude::*,
+    render::{
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        camera::RenderTarget,
+        camera::Viewport
+    },
+};
+
 
 use crate::{
-    asset_loader::SceneAssets,
-    collision_detection::{Collider, CollisionDamage},
-    health::Health,
-    movement::{Acceleration, MovingObjectBundle, Velocity},
-    schedule::InGameSet,
-    state::GameState,
+  asset_loader::SceneAssets,
+  collision_detection::{Collider, CollisionDamage},
+  health::Health,
+  movement::{Acceleration, MovingObjectBundle, Velocity},
+  schedule::InGameSet,
+  state::GameState,
 };
+
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const SPACESHIP_RADIUS: f32 = 5.0;
@@ -31,7 +43,14 @@ pub struct SpaceshipShield;
 #[derive(Component, Debug)]
 pub struct SpaceshipMissile;
 
+#[derive(Component, Debug)]
+pub struct Vision
+{
+  handle: Handle<Image>
+}
+
 pub struct SpaceshipPlugin;
+
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
@@ -51,8 +70,48 @@ impl Plugin for SpaceshipPlugin {
     }
 }
 
-fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
-    commands.spawn((
+
+fn create_spaceship_vision() -> Image
+{
+    let size = Extent3d {
+        width: 50,
+        height: 200,
+        ..default()
+    };
+
+    // This is the texture that will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+//                | TextureUsages::COPY_SRC
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    // fill image.data with zeroes
+    image.resize(size);
+
+    image
+}
+
+
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>, mut images: ResMut<Assets<Image>>)
+
+{
+    let vision = create_spaceship_vision();
+    let handle = images.add(vision);
+
+    let vision_image_clone = handle.clone();
+    let parent_id = commands.spawn((
         MovingObjectBundle {
             velocity: Velocity::new(Vec3::ZERO),
             acceleration: Acceleration::new(Vec3::ZERO),
@@ -64,10 +123,37 @@ fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
             },
         },
         Spaceship,
+        Vision { handle },
         Health::new(SPACESHIP_HEALTH),
         CollisionDamage::new(SPACESHIP_COLLISION_DAMAGE),
-    ));
+    )).id();
+
+
+    let camera_id = commands.spawn(Camera3dBundle {
+        camera_3d: Camera3d {
+            clear_color: ClearColorConfig::None,
+
+            ..default()
+        },
+        camera: Camera {
+          // render before the "main pass" camera
+          order: 2,
+//            target: RenderTarget::Image(vision_image_clone),
+          viewport: Some(Viewport {
+            physical_position: UVec2::new(0, 0),
+            physical_size: UVec2::new(256, 256),
+            ..default()
+          }),
+          ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 1.0, 8.0))
+            .looking_at(Vec3::new(0.0, 1.0, 30.), -Vec3::Y),
+        ..default()
+    }).id();
+
+    commands.entity(parent_id).push_children(&[camera_id]);
 }
+
 
 fn spaceship_movement_controls(
     mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>,
@@ -110,6 +196,7 @@ fn spaceship_movement_controls(
     // Update the spaceship's velocity based on new direction.
     velocity.value = -transform.forward() * movement;
 }
+
 
 fn spaceship_weapon_controls(
     mut commands: Commands,
