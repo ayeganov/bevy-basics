7ce0ed6838dfa66c9e2a67ce5b9b3b9c97c03945
Aleksandr Yeganov <ayeganov@gmail.com>
2024-03-17 11:57:52 -0400

    junk commit

--- episode-5/src/ai_framework.rs
+++ episode-5/src/ai_framework.rs
@@ -19,24 +19,24 @@ pub enum Sensors
 #[derive(Component, Debug)]
 pub enum Environment
 {
-  Visible(VisibleEnvironment)
+  VisibleEnvironment
 }
 
 
 trait Sensing
 {
-  fn sense(&self, environment: Environment) -> Option<Vec<f32>>;
+  fn sense(&self, environment: Environment, images: Res<Assets<Image>>) -> Option<Vec<f32>>;
 }
 
 
 impl Sensing for VisionSensor
 {
-  fn sense(&self, environment: Environment) -> Option<Vec<f32>>
+  fn sense(&self, environment: Environment, images: Res<Assets<Image>>) -> Option<Vec<f32>>
   {
     match environment
     {
-      Environment::Visible(visible_env) => {
-        if let Some(image) = visible_env.image_assets.get(&self.visual_sensor)
+      Environment::VisibleEnvironment => {
+        if let Some(image) = images.get(&self.visual_sensor)
         {
           let data = image.data.iter().map(|&b| b as f32).collect();
           return Some(data);
@@ -53,13 +53,6 @@ impl Sensing for VisionSensor
 struct Agent<S: Sensing>
 {
   sensor: S,
-}
-
-
-#[derive(Component, Debug)]
-struct VisibleEnvironment
-{
-  image_assets: Res<'static, Assets<Image>>,
 }
 
 
